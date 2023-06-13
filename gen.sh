14bb9a91b5fec8e7e4388a420ad5f4c0da083bde
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2023-06-14 00:44:38 +1000

    gen script

--- gen.sh
+++ gen.sh
@@ -5,9 +5,9 @@ TCLB=master # This can be any folder with TCLB
 RT=$TCLB/tools/RT
 LG=$TCLB/CLB/d3q27_PSM_NEBB/lammps
 
-# $RT -f frac_my.Rt
-#$RT -f pack.lammps.Rt -o data/pack.lammps
-#$LG data/pack.lammps
-#R -f pack_copy.R
+$RT -f frac_my.Rt
+$RT -f pack.lammps.Rt -o data/pack.lammps
+$LG data/pack.lammps
+R -f pack_copy.R
 $RT -f flow.xml.Rt
 $RT -f flow.lammps.Rt
