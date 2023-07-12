b496230f7f1a6ff5ee2fb382fef888d640701d90
L <uqllanie@uq.edu.au>
2023-07-12 19:51:58 +0800

    update

---
+++ all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./install.sh singlekernel llaniewski feature/singlekernel LAMMPS=/scratch/director2188/sprint/LIGGGHTS-PUBLIC
+./install.sh fastdem llaniewski feature/fastdem LAMMPS=/scratch/director2188/sprint/LIGGGHTS-PUBLIC
+./install.sh develop CFD-GO develop LAMMPS=/scratch/director2188/sprint/LIGGGHTS-PUBLIC
