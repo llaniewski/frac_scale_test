62ef1a80e2657eb6d0d4c230e9a76ec3c6466609
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2023-07-12 19:53:14 +0800

    update of topaz scirpt

--- topaz/test.sh
+++ topaz/test.sh
@@ -21,6 +21,10 @@ module load cuda/11.4.2
 module load r/4.0.2
 module load openmpi-ucx-gpu/4.0.2
 
+i=flow_0_1_1
+CLB/d3q27_PSM_NEBB/main data/$i.xml
+
+
 for i in flow_3000_1_1 flow_6000_1_1 flow_9000_1_1 flow_12000_1_1 flow_15000_1_1 flow_18000_1_1 flow_21000_1_1 flow_24000_1_1 flow_27000_1_1
 do
 	mpirun -np 1 CLB/d3q27_PSM_NEBB/main data/$i.xml : -np 4 CLB/d3q27_PSM_NEBB/lammps data/$i.lammps
