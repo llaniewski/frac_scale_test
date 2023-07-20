f9cebc93e9a0979af6264eb60d519cf96bb0f1c3
L <uqllanie@uq.edu.au>
2023-07-20 12:09:51 +0800

    Adding interior to tests

--- all.sh
+++ all.sh
@@ -5,6 +5,6 @@ then
 	ARG="LAMMPS=$1"
 fi
 ./install.sh singlekernel llaniewski feature/singlekernel $ARG
-./install.sh singlekernel interior feature/interior $ARG
+./install.sh interior llaniewski feature/interior $ARG
 ./install.sh fastdem llaniewski feature/fastdem $ARG
 ./install.sh develop CFD-GO develop $ARG
